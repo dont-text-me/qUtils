909b7ee68579b467ed069af56c641c460885c856
dont-text-me <ivanbar3330@gmail.com>
2025-02-20 19:33:29 +0000

    add shrinker to complex number generator

--- src/test/kotlin/utils/ArbitraryComplex.kt
+++ src/test/kotlin/utils/ArbitraryComplex.kt
@@ -1,12 +1,20 @@
 package utils
 
+import complex.ComplexNumber
+import complex.ComplexNumber.Companion.i
 import complex.ComplexNumberCart
 import io.kotest.property.Arb
+import io.kotest.property.Shrinker
 import io.kotest.property.arbitrary.arbitrary
 import io.kotest.property.arbitrary.double
 
+private val shrinker =
+    Shrinker<ComplexNumber> { num ->
+        listOf(num + 0.5, num - 0.5, num + 0.5.i, num - 0.5.i)
+    }
+
 val arbitraryComplex =
-    arbitrary {
+    arbitrary(shrinker) {
         val real = Arb.double().bind()
         val im = Arb.double().bind()
         ComplexNumberCart(real, im)
